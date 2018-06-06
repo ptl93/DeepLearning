89392ea63d91329ed9d36dc0459e0ce49b6f050d
ptl93 <tuan.le@campus.lmu.de>
2018-06-06 19:40:55 +0200

    add extra dataset and own holdout resampling

--- StreetViewHouseNumbers/source/loader.py
+++ StreetViewHouseNumbers/source/loader.py
@@ -13,16 +13,17 @@ from os.path import dirname
 import numpy as np
 import scipy.io as sio
 from keras.utils import np_utils
-
+from sklearn.model_selection import train_test_split
+from math import ceil
 #Define location of datasets
 train_data = f"{dirname(__file__)}/../data/train_32x32.mat"
 test_data = f"{dirname(__file__)}/../data/test_32x32.mat"
 extra_data = f"{dirname(__file__)}/../data/extra_32x32.mat"
 
 #Testing:
-train_data = "C:/Users/tuan.le/Desktop/DeepLearning/StreetViewHouseNumbers/data/train_32x32.mat"
-test_data = "C:/Users/tuan.le/Desktop/DeepLearning/StreetViewHouseNumbers/data/test_32x32.mat"
-extra_data = "C:/Users/tuan.le/Desktop/DeepLearning/StreetViewHouseNumbers/data/extra_32x32.mat"
+#train_data = "C:/Users/tuan.le/Desktop/DeepLearning/StreetViewHouseNumbers/data/train_32x32.mat"
+#test_data = "C:/Users/tuan.le/Desktop/DeepLearning/StreetViewHouseNumbers/data/test_32x32.mat"
+#extra_data = "C:/Users/tuan.le/Desktop/DeepLearning/StreetViewHouseNumbers/data/extra_32x32.mat"
 
 def load_data(load_extra_data = False):
     
@@ -39,37 +40,76 @@ def load_data(load_extra_data = False):
     train = sio.loadmat(train_data)
     ## From dictionary train extract the X and y components
     X_train = train["X"]
-    ## Scale X_train into range (0,1)
-    X_train = X_train / np.max(X_train)
-    ## 1-0 Hot encoding into matrix for each class
-    y_train = np_utils.to_categorical(train["y"])
-    ## Put into tuple
-    train_processed = (X_train, y_train)
-    print("Shape from X_train:", X_train.shape)
-    print("Number of target classes:", y_train.shape[1])
-    
+    ## Get y_train
+    y_train = train["y"]
     ## Load Test Set
     test = sio.loadmat(test_data)
     ## From dictionary train extract the X and y components
     X_test = test["X"]
-    ## Scale X_test into range (0,1)
-    X_test = X_test / np.max(X_test)
-    #1-0 Hot encoding into matrix for each class
-    y_test = np_utils.to_categorical(test["y"])
-    ## Put into tuple
-    test_processed = (X_test, y_test)
-    print("Shape from X_test:", X_test.shape)
-    print("Number of target classes:", y_test.shape[1])
+    # Get y_test
+    y_test = test["y"]
+    del train, test    
+    #if only train and test: 
+    #train 73257 observations, test 26032 observation: test-train ratio approc: 0.355 -- better use 0.15
+    ## train_test_split problematic
+    ## stratification might be a problem. lets run it first and see... 
+    if load_extra_data == False: 
+        X = np.concatenate((X_train, X_test), axis = 3)
+        #scale into range (0,1)
+        X = X / np.max(X)
+        y = np.concatenate((y_train, y_test), axis = 0)
+        y = np_utils.to_categorical(y)
+        del X_train, X_test
+        del y_train, y_test
+        n_obs = X.shape[3]
+        test_size = ceil(n_obs * 0.15)
+        test_idx = np.random.choice(n_obs, size = test_size, replace = False)
+        train_idx = np.setdiff1d(np.arange(n_obs), test_idx, assume_unique = True)
+        X_train = X[:,:,:,train_idx]
+        y_train = y[train_idx,:]
+        X_test = X[:,:,:,test_idx]
+        y_test = y[test_idx,:]
+        del X, y
+        
+        train_processed = (X_train, y_train)
+        print("Shape from X_train:", X_train.shape)
+        print("Number of target classes:", y_train.shape[1])
+        test_processed = (X_test, y_test)
+        print("Shape from X_test:", X_test.shape)
+        print("Number of target classes:", y_test.shape[1])
+        
     # Load Extra dataset as additional training data if necessary
-    if load_extra_data:
-        extra = sio.loadmat(train_data)
+    if load_extra_data == True: ##might get problematic because of working memory
+        extra = sio.loadmat(extra_data)
         ## From dictionary train extract the X and y components
         X_extra = extra["X"]
-        ## Scale X_extra into range (0,1)
-        X_extra = X_extra / np.max(X_extra)
-        ## 1-0 Hot encoding into matrix for each class
-        y_extra = np_utils.to_categorical(extra["y"])  
-        extra = (X_extra, y_extra)
-    else:
-        extra = None
-    return train_processed, test_processed, extra
+        ## get y_extra
+        y_extra = extra["y"]
+        del extra
+        X = np.concatenate((X_train, X_test, X_extra), axis = 3)
+        del X_train, X_test, X_extra
+        y = np.concatenate((y_train, y_test, y_extra), axis = 0)
+        del y_train, y_test, y_extra
+        y = np_utils.to_categorical(y)
+        
+        n_obs = X.shape[3]
+        test_size = ceil(n_obs * 0.15)
+        test_idx = np.random.choice(n_obs, size = test_size, replace = False)
+        train_idx = np.setdiff1d(np.arange(n_obs), test_idx, assume_unique = True)
+        # scale into range (0,1)
+        X = X / np.max(X) 
+        ## the division by 255 is problematic. always crashes!
+        X_train = X[:,:,:,train_idx]
+        y_train = y[train_idx,:]
+        X_test = X[:,:,:,test_idx]
+        y_test = y[test_idx,:]
+        del X, y
+        
+        train_processed = (X_train, y_train)
+        print("Shape from X_train:", X_train.shape)
+        print("Number of target classes:", y_train.shape[1])
+        test_processed = (X_test, y_test)
+        print("Shape from X_test:", X_test.shape)
+        print("Number of target classes:", y_test.shape[1])
+        
+    return train_processed, test_processed
